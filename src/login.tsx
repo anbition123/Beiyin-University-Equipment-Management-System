b61715f4b5d45cd0b53bb62859f4f053e1624588
wxchengting <wxchengting@gf.com.cn>
2022-11-02 16:26:00 +0800

    第一次提交

--- src/login.tsx
+++ src/login.tsx
@@ -6,7 +6,7 @@ function Login() {
     <div className="login">
       <p className="login-header">北京印刷学院印设备管理系统</p>
       <div className="login_disc">
-        <p>老师登录</p><p>学生登录</p>
+        <p>老师登录</p> <p>学生登录</p>
         <input></input>
         <input></input>
         <input></input>
