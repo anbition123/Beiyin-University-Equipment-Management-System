ed02d3777e5515fe4d9bd58eb55881bc6560747d
wxchengting <wxchengting@gf.com.cn>
2022-11-02 15:14:37 +0800

    初建设备平台

---
+++ src/login.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './login.css';
+
+function Login() {
+  return (
+    <div className="login">
+      <p className="login-header">北京印刷学院印设备管理系统</p>
+      <div className="login_disc">
+        <p>登录</p>
+        <input></input>
+        <input></input>
+        <input></input>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Login;
